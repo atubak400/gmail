d88f68d44b559d338419711fbb11838ec0a23e64
Kingsley Atuba <atubak40@gmail.com>
2021-07-15 16:11:46 +0100

    created my components

---
+++ gmail-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Header from './header'
+
+function App() {
+	return (
+		<div className='app'>
+			<Header />
+			<h1>Lets Build GMAIL</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ gmail-clone/src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+function header() {
+	return (
+		<div className='header'>
+			<h2>This is a header</h2>
+		</div>
+	)
+}
+
+export default header
