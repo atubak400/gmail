f41ed3c06b8d13d47e7f7321be2a52151775cdba
Kingsley Atuba <atubak40@gmail.com>
2021-07-30 11:28:28 +0100

    sidebar footer done

--- gmail-clone/src/Sidebar.js
+++ gmail-clone/src/Sidebar.js
@@ -1,5 +1,5 @@
 import './sidebar.css'
-import { Button } from '@material-ui/core'
+import { Button, IconButton } from '@material-ui/core'
 import React from 'react'
 import AddIcon from '@material-ui/icons/Add'
 import InboxIcon from '@material-ui/icons/Inbox'
@@ -10,6 +10,9 @@ import LabelImportantIcon from '@material-ui/icons/LabelImportant'
 import NearMeIcon from '@material-ui/icons/NearMe'
 import NoteIcon from '@material-ui/icons/Note'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import PersonIcon from '@material-ui/icons/Person'
+import DuoIcon from '@material-ui/icons/Duo'
+import PhoneIcon from '@material-ui/icons/Phone'
 
 function Sidebar() {
   return (
@@ -33,7 +36,17 @@ function Sidebar() {
       <SidebarOptions Icon={NoteIcon} title='Drafts' number={54} />
       <SidebarOptions Icon={ExpandMoreIcon} title='More' number={54} />
       <div className='sidebar__footer'>
-        <div className='sidebar__footerIcons'></div>
+        <div className='sidebar__footerIcons'>
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   )
