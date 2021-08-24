3a5bb916461990cee8113e890df31b199f2022bc
Kingsley Atuba <atubak40@gmail.com>
2021-08-24 10:46:44 +0100

    email roe component done

--- gmail-clone/src/EmailList.js
+++ gmail-clone/src/EmailList.js
@@ -12,6 +12,7 @@ import LocalOfferIcon from '@material-ui/icons/LocalOffer'
 import React from 'react'
 import './emailList.css'
 import Section from './Section'
+import EmailRow from './EmailRow'
 
 function EmailList() {
   return (
@@ -48,6 +49,22 @@ function EmailList() {
         <Section Icon={InboxIcon} title='Primary' color='red' selected />
         <Section Icon={PeopleIcon} title='Social' color='#1A73E8' />
         <Section Icon={LocalOfferIcon} title='Promotions' color='green' />
+      </div>
+      <div className='emailList__list'>
+        <EmailRow
+          title='Twitch'
+          subject='Hey follow me'
+          description='This is a test'
+          time='10pm'
+        />
+      </div>
+      <div className='emailList__list'>
+        <EmailRow
+          title='Twitch'
+          subject='Hey follow Kingsley Atuba'
+          description='This is a test'
+          time='10pm'
+        />
       </div>
     </div>
   )
